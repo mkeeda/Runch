245c2cc80cbcaeae018518875cdedfb445f57196
IppeiMukaida <ippei-mukaida@cybozu.co.jp>
2019-04-07 02:21:00 +0900

    Add loginActivity

--- app/src/main/java/com/mkeeda/runch/RootActivity.kt
+++ app/src/main/java/com/mkeeda/runch/RootActivity.kt
@@ -1,12 +1,17 @@
 package com.mkeeda.runch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mkeeda.runch.view.login.LoginActivity
 
 class RootActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
+
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        this.startActivity(loginIntent)
     }
 }
