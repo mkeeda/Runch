e6231df26b8c015ca6b94b8c95c3002327c18dcd
IppeiMukaida <ippei-mukaida@cybozu.co.jp>
2019-04-19 01:15:28 +0900

    Create GnaviApiRequestInterceptor.kt

---
+++ app/src/main/java/com/mkeeda/runch/api/gnavi/GnaviApiRequestInterceptor.kt
@@ -0,0 +1,25 @@
+package com.mkeeda.runch.api.gnavi
+
+import com.mkeeda.runch.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class GnaviApiRequestInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val originalUrl = originalRequest
+            .url()
+            .newBuilder()
+        val apiKey = BuildConfig.gnaviApiKey
+
+        val newUrl = originalUrl
+            .addQueryParameter("keyid", apiKey)
+            .build()
+        val newRequest = originalRequest
+            .newBuilder()
+            .url(newUrl)
+            .build()
+        return chain.proceed(newRequest)
+    }
+
+}
