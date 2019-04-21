70cb6974419cda074915c8d3cf63040c3e0adb14
IppeiMukaida <ippei-mukaida@cybozu.co.jp>
2019-04-21 17:01:46 +0900

    Create RestSearchRepository

--- app/src/main/java/com/mkeeda/runch/RootActivity.kt
+++ app/src/main/java/com/mkeeda/runch/RootActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import com.mkeeda.runch.api.gnavi.GnaviApiRequestInterceptor
-import com.mkeeda.runch.api.gnavi.RestSearchService
+import com.mkeeda.runch.domainimpl.repository.RestSearchRepositoryImpl
 import com.mkeeda.runchdomain.extension.disposed
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -45,15 +43,10 @@ class RootActivity : AppCompatActivity() {
             .client(httpClientBuilder.build())
             .build()
 
-        val restSearchService = retrofit.create(RestSearchService::class.java)
-        restSearchService.searchRestByLocation(latitude = 35.682444, longitude = 139.773611)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                println("⭐$it")
-            }, {
-                println("⭐$it")
-            }).disposed(by = disposeBag)
+        RestSearchRepositoryImpl(retrofit = retrofit).retrieveRandom5ByLocation(latitude = 35.682444, longitude = 139.773611)
+            .subscribe { item ->
+                println("⭐$item")
+            }.disposed(by = disposeBag)
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/com/mkeeda/runch/domainimpl/repository/RestSearchRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.mkeeda.runch.domainimpl.repository
+
+import com.mkeeda.runch.api.gnavi.RestSearchService
+import com.mkeeda.runchdomain.entity.Restaurant
+import com.mkeeda.runchdomain.repository.RestSearchRepository
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+
+class RestSearchRepositoryImpl: RestSearchRepository {
+    private val restSearchService: RestSearchService
+
+    constructor(retrofit: Retrofit) {
+        this.restSearchService = retrofit.create(RestSearchService::class.java)
+    }
+    override fun retrieveRandom5ByLocation(latitude: Double, longitude: Double): Single<List<Restaurant>> {
+        return restSearchService.searchRestByLocation(latitude = latitude, longitude = longitude)
+            .map {
+                it.rest
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+}
