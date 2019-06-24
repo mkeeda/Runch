a9e0bd8c520c870a13813354d389954f5df52a81
IppeiMukaida <ippei-mukaida@cybozu.co.jp>
2019-06-24 12:50:34 +0900

    Show shop images

--- app/src/main/java/com/mkeeda/runch/view/restlist/RestCardRecyclerViewAdapter.kt
+++ app/src/main/java/com/mkeeda/runch/view/restlist/RestCardRecyclerViewAdapter.kt
@@ -3,16 +3,20 @@ package com.mkeeda.runch.view.restlist
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.mkeeda.runch.R
 import com.mkeeda.runchdomain.entity.Restaurant
 import kotlinx.android.synthetic.main.rest_card.view.rest_card_access_walk
 import kotlinx.android.synthetic.main.rest_card.view.rest_card_category
 import kotlinx.android.synthetic.main.rest_card.view.rest_card_name
+import kotlinx.android.synthetic.main.rest_card.view.rest_card_thumbnail_image
 
 class RestCardRecyclerViewAdapter: RecyclerView.Adapter<RestCardViewHolder>() {
+    private var parent: ViewGroup? = null
     private var restList: List<Restaurant> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestCardViewHolder {
+        this.parent = parent
         val layoutView = LayoutInflater.from(parent.context).inflate(R.layout.rest_card, parent, false)
         return RestCardViewHolder(layoutView)
     }
@@ -22,9 +26,21 @@ class RestCardRecyclerViewAdapter: RecyclerView.Adapter<RestCardViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: RestCardViewHolder, position: Int) {
-        holder.itemView.rest_card_name.text = restList[position].name
-        holder.itemView.rest_card_category.text = restList[position].category
-        holder.itemView.rest_card_access_walk.text = restList[position].access.walk
+        val rest = restList[position]
+        holder.itemView.rest_card_name.text = rest.name
+        holder.itemView.rest_card_category.text = rest.category
+        holder.itemView.rest_card_access_walk.text = rest.access.walk
+
+        val thumbnailUrl = if (rest.image_url.shop_image1.isNotEmpty()) {
+            rest.image_url.shop_image1
+        } else {
+            rest.image_url.shop_image2
+        }
+        this.parent?.let {
+            Glide.with(it.context)
+                .load(thumbnailUrl)
+                .into(holder.itemView.rest_card_thumbnail_image);
+        }
     }
 
     fun update(restList: List<Restaurant>) {
