20e94e1d31e18eb66280b86a059dfc11b4c32982
IppeiMukaida <ippei-mukaida@cybozu.co.jp>
2019-10-05 19:54:28 +0900

    Remove custom ViewModelFactory, use only RunchViewModelFactory

--- app/src/main/java/com/mkeeda/runch/view/restlist/RestListFragment.kt
+++ app/src/main/java/com/mkeeda/runch/view/restlist/RestListFragment.kt
@@ -5,14 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import com.mkeeda.runch.R
+import com.mkeeda.runch.RunchApplication
+import com.mkeeda.runch.domainimpl.repository.RestSearchRepositoryImpl
+import com.mkeeda.runch.util.RunchViewModelFactory
+import com.mkeeda.runchdomain.usecase.ShowRestUseCase
 import kotlinx.android.synthetic.main.rest_list_fragment.view.restlist_recycler_view
 
 class RestListFragment : Fragment() {
-    private val viewModel: RestListViewModel by lazy {
-        ViewModelProvider(this, RestListViewModelFactory()).get(RestListViewModel::class.java)
+    private val viewModel: RestListViewModel by viewModels {
+        RunchViewModelFactory {
+            val repository = RestSearchRepositoryImpl(retrofit = RunchApplication.singleton.retrofit)
+            val showRestUseCase = ShowRestUseCase(restSearchRepository = repository)
+            RestListViewModel(showRestUseCase = showRestUseCase)
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater,
